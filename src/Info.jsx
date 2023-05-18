be873b92a60792f3526dd76615057536a1f6276f
Gresham24 <tembogresham@gmail.com>
2023-05-18 18:08:42 +0200

    created component files

---
+++ src/Info.jsx
@@ -0,0 +1,26 @@
+import "./Info.css";
+
+function Info() {
+    return (
+        <>
+            <h1>Vite + React</h1>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
+}
+
+export default Info;
+
+
+
+/* 
+
+Create separate components for:
+- info (photo, name, buttons)
+- about 
+- interests
+- footer (social icons)
+
+*/
