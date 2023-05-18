11597fd731117582294fd7958ca47b9657b6c5be
Gresham24 <tembogresham@gmail.com>
2023-05-18 18:28:48 +0200

    styled root

--- src/Info.jsx
+++ src/Info.jsx
@@ -2,12 +2,12 @@ import "./Info.css";
 
 function Info() {
     return (
-        <>
+        <div className="info">
             <h1>Vite + React</h1>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
-        </>
+        </div>
     );
 }
 
