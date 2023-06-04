8c225aba6eb1f57ec48aabbed5b20efb2852cafc
Gresham24 <tembogresham@gmail.com>
2023-06-04 17:04:21 +0200

    added and styled footer icons

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <img src="/src/assets/Twitter Icon.png" alt="twitter icon" />
+            <img src="/src/assets/Facebook Icon.png" alt="Facebook icon" />
+            <img src="/src/assets/Instagram Icon.png" alt="Instagram icon" />
+            <img src="/src/assets/GitHub Icon.png" alt="GitHub icon" />
+        </div>
+    );
+}
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Info from "./Info.jsx";
 import About from "./About.jsx";
+import Footer from "./Footer.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <Info />
         <About />
+        <Footer />
     </React.StrictMode>
 );
 
