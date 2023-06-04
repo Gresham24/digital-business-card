01e14306be41b580fd272b80973e27abb5dd781c
Gresham24 <tembogresham@gmail.com>
2023-06-04 16:46:09 +0200

    added about section

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Info from "./Info.jsx";
+import About from "./About.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <Info />
+        <About />
     </React.StrictMode>
 );
 
